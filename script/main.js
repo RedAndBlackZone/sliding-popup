139c18882e04f6d7f12f0a65de3a9c5a0da9896d
BigSteveLittle <bigsteve@redandblackzone.com>
2022-03-10 13:14:46 +1100

    Sets up initial JS variables and listeners.

--- script/main.js
+++ script/main.js
@@ -1,3 +1,10 @@
 // APP: SLIDING OPTION POPUP.
 // Invoke 'strict mode'.
 "use strict";
+// Set up HTML element variables.
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+// Event listeners.
+signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
+signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
